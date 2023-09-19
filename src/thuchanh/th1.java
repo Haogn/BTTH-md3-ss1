6dab41710d2001478e71dfd09e658678dda1b7f3
unknown <nshoang270298@gmail.com>
2023-09-19 22:25:55 +0700

    BTTH md3-ss1

---
+++ src/thuchanh/th1.java
@@ -0,0 +1,11 @@
+package thuchanh;
+
+import java.util.Date;
+
+public class th1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println("Time now: " + date);
+    }
+}
